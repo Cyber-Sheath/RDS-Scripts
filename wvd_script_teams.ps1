5d425b4266e1b33932cd457221d60e580c323b3e
john-woods-cybersheath <71682438+john-woods-cybersheath@users.noreply.github.com>
2020-09-21 22:50:29 -0400

    Add files via upload

---
+++ wvd_script_teams.ps1
@@ -0,0 +1,16 @@
+﻿$tenantName= "nrt"
+$hostpool = "pool1"
+#Get-RdsHostPool -TenantName $tenantName -Name $hostpool
+#Get-RdsAppGroup -TenantName $tenantName -HostPoolName $hostpool
+$Thumbprint = "BBBC38ED74F21B8427B162AF80A4F17282D8340F"
+$TenantId = "5231c81b-959c-4621-a7bd-6635103652e7"
+$ApplicationId = "21cb4b5f-f455-4333-b6ac-86671ffa987b"
+Add-RdsAccount -DeploymentUrl "https://rdbroker.wvd.microsoft.com"
+#Get-RdsAppGroup -TenantName $tenantName -HostPoolName $hostpool
+Get-RdsHostPool -TenantName $tenantName -HostPoolName $hostpool
+Get-RdsSessionHost -TenantName $tenantName -HostPoolName $hostpool
+#$Credential = Get-Credential
+#add-rdsaccount -deploymenturl "https://rdbroker.wvd.microsoft.com"
+#john.woods.adm@nrtbus.onmicrosoft.com
+#cybjoW*0102
+
